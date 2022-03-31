b81e280152000613661f29cdf185395f0a07f9fc
Emir Saglik <emirkaansaglik@hotmail.de>
2022-03-31 12:58:29 +0200

    added .json serialization

--- StorageShortageWarningSystem/Program.cs
+++ StorageShortageWarningSystem/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Text.Json;
 
 namespace StorageShortageWarningSystem
 {
@@ -7,19 +9,25 @@ namespace StorageShortageWarningSystem
     {
         static void Main(string[] args)
         {
+            List<Drive> _drives = new List<Drive>();
             DriveInfo[] allDrives = DriveInfo.GetDrives();
+
 
             foreach (DriveInfo d in allDrives) 
             {
                 if (d.DriveType.Equals(DriveType.Fixed))
                 {
-                    Console.WriteLine(d.Name);
-                    Console.WriteLine(d.TotalSize);
-                    Console.WriteLine(d.AvailableFreeSpace);
+                    _drives.Add(new Drive()
+                    {
+                        Name = d.Name,
+                        TotalSize = d.TotalSize,
+                        AvailableFreeSpace = d.AvailableFreeSpace
+                    });
                 }
-
-
             }
+            string json = JsonSerializer.Serialize(_drives);
+            File.WriteAllText("./drives.json", json);
+
         }
     }
 }
