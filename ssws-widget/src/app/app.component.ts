e1dbb03429e17f6850d62b187a7b2b78b96680f7
Emir Saglik <emirkaansaglik@hotmail.de>
2022-03-31 13:44:44 +0200

    added drive-widget

--- ssws-widget/src/app/app.component.ts
+++ ssws-widget/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {drives }
 
 @Component({
   selector: 'app-root',
--- ssws-widget/src/app/drive-widget/drive-widget.component.ts
+++ ssws-widget/src/app/drive-widget/drive-widget.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { drives } from '../../assets/drives.json'
 
 @Component({
   selector: 'app-drive-widget',
