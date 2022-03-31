2479db14ed7925b4b0c4c590e2152a793a31687b
Emir Kaan Saglik <emir.saglik@serviceware.de>
2022-03-31 12:13:32 +0200

    init

---
+++ StorageShortageWarningSystem/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace StorageShortageWarningSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+            foreach (DriveInfo d in allDrives) 
+            {
+                if (d.DriveType.Equals(DriveType.Fixed))
+                {
+                    Console.WriteLine(d.Name);
+                    Console.WriteLine(d.TotalSize);
+                    Console.WriteLine(d.AvailableFreeSpace);
+                }
+
+
+            }
+        }
+    }
+}
