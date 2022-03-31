cf0c741fa8f162d24ebc06f6f4a38dac1d903687
Emir Saglik <emirkaansaglik@hotmail.de>
2022-03-31 12:54:59 +0200

    Added Drive class

---
+++ StorageShortageWarningSystem/Drive.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace StorageShortageWarningSystem
+{
+    public class Drive
+    {
+        public string Name { get; set; }
+        public long TotalSize { get; set; }
+        public long AvailableFreeSpace { get; set; }
+        
+    }
+}
